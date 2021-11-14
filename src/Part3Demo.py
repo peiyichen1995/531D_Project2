70da5497e97e9b3b33f0dda1cfa4e8934f2804fc
yuboding3 <casselyou@gmail.com>
2021-11-14 12:23:52 -0500

    Update Part3Demo.py

--- src/Part3Demo.py
+++ src/Part3Demo.py
@@ -5,5 +5,5 @@ budgets, queries, bids=tool.readData('../data/ds3.pkl')
 print(budgets.shape,queries.shape,bids.shape)
 temp=extension4(budgets, queries, bids,0.02)
 print(temp)
-
+print()
 print(greedyOnlineAdWord(budgets, queries, bids)[1])
